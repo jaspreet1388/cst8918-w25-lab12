24a05633f2dce8566f0b3cfef2403fdd4bbfa69d
jaspreet1388 <chha0038@algonquinlive.com>
2025-08-07 21:40:52 -0400

    Update terraform.tf

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,17 +1,16 @@
 terraform {
   required_version = "~> 1.5"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.96.0"
     }
   }
-
   backend "azurerm" {
+    resource_group_name  = "chha0038-githubactions-rg"
     storage_account_name = "chha0038githubactionslab"
     container_name       = "tfstate"
-    key                  = "prod.app.tfstate"
+    key                  = "tf-app/terraform.tfstate"
     use_oidc             = true
   }
 }
@@ -19,4 +18,23 @@ terraform {
 provider "azurerm" {
   features {}
   use_oidc = true
+}
+
+variable "location" {
+  type    = string
+  default = "canadacentral"
+}
+
+variable "college_id" {
+  type    = string
+  default = "chha0038"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.college_id}-app-rg"
+  location = var.location
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
